898918389c9a17bd82f46e6adc4fe89c9fa4f30a
sandesh <sandeshyapuram@gmail.com>
2018-09-23 10:55:01 +0530

    Place order

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -4,9 +4,9 @@ const mbxDirections = require('@mapbox/mapbox-sdk/services/directions');
 const directionsClient = mbxDirections({ accessToken: 'pk.eyJ1Ijoic2FuZGVzaHlhcHVyYW0iLCJhIjoiY2ptZGw2bXFxNjJxazNxbGkxNmFzNWxlaSJ9.Xr1eZTKMDJnlayXvr20Q8w' });
 
 module.exports = function (app, db) {
-  
+
   app.post('/order', (req, res) => {
-    
+
     directionsClient
       .getDirections({
         waypoints: [
@@ -19,22 +19,30 @@ module.exports = function (app, db) {
         ]
       })
       .send()
-      .then( response => {
-        
-        const note = { asdas: 123 }
+      .then(response => {
+        const distance = JSON.parse(response.rawBody).routes[0].distance;
+        const originCoord = req.body.origin.map(Number);
+        const destCoord = req.body.destination.map(Number);
+        const note = { "distance": distance, "originCoord": originCoord, "destCoord": destCoord, "status": "UNASSIGN" }
         db.collection('notes').insert(note, (err, result) => {
           if (err) {
             res.send({ 'error': 'An error has occurred' });
           } else {
-            res.send(result.ops[0]);
+            console.log(result)
+            const responseString = {
+              "id": result.ops[0]._id,
+              "distance": result.ops[0].distance,
+              "status": result.ops[0].status
+            }
+            res.send(responseString);
           }
         });
       }).catch((error) => {
+        console.log(error)
         res.send({ 'error': 'An error has occurred' })
-       // done();
+
       });
-    //console.log(directions)
-    
+
 
   });
 };

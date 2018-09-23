0847fa47b2243fdaa696f5602f5427202e2ba18b
sandesh <sandeshyapuram@gmail.com>
2018-09-23 10:24:26 +0530

    3

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,5 @@
 var ObjectID = require('mongodb').ObjectID;
+
 const mbxDirections = require('@mapbox/mapbox-sdk/services/directions');
 const directionsClient = mbxDirections({ accessToken: 'pk.eyJ1Ijoic2FuZGVzaHlhcHVyYW0iLCJhIjoiY2ptZGw2bXFxNjJxazNxbGkxNmFzNWxlaSJ9.Xr1eZTKMDJnlayXvr20Q8w' });
 
@@ -18,31 +19,22 @@ module.exports = function (app, db) {
         ]
       })
       .send()
-      .then(getDirection, response => {
+      .then( response => {
         
-        getDirection(response.body, req.body.origin.map(Number), req.body.destination.map(Number));
-        //  console.log(directions)
+        const note = { asdas: 123 }
+        db.collection('notes').insert(note, (err, result) => {
+          if (err) {
+            res.send({ 'error': 'An error has occurred' });
+          } else {
+            res.send(result.ops[0]);
+          }
+        });
       }).catch((error) => {
         res.send({ 'error': 'An error has occurred' })
-        done();
+       // done();
       });
     //console.log(directions)
-    const note = { asdas: 123 }
+    
 
   });
-
-  function getDirection(directions, orgCoord, destCoord) {
-    console.log(directions)
-    const note = {
-      
-    }
-    db.collection('notes').insert(note, (err, result) => {
-      if (err) {
-        res.send({ 'error': 'An error has occurred' });
-      } else {
-        res.send(result.ops[0]);
-      }
-    });
-  }
-
 };
